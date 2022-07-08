ac8246eed4b3eb6cda4fa5472398332c778878b7
yukitteru.a <yukitteru.a@gmail.com>
2022-07-09 02:15:31 +0500

    Sorting by desc

--- src/main/java/com/temnet/parser/service/ArchiveService.java
+++ src/main/java/com/temnet/parser/service/ArchiveService.java
@@ -13,11 +13,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @Transactional
@@ -32,9 +37,10 @@ public class ArchiveService {
     }
 
     public Page<Report> getReport(Page<Archive> archive) {
-        LinkedHashMap<String, Long> resultMap = new LinkedHashMap<>();
+        LinkedHashMap<String, Long> stringLongLinkedHashMap = new LinkedHashMap<>();
         List<Report> report = new ArrayList<>();
-        archive.forEach(a -> resultMap.put(a.getUsername(), resultMap.getOrDefault(a.getUsername(), 0L) + 1L));
+        archive.forEach(a -> stringLongLinkedHashMap.put(a.getUsername(), stringLongLinkedHashMap.getOrDefault(a.getUsername(), 0L) + 1L));
+        Map<String, Long> resultMap = orderByValue(stringLongLinkedHashMap);
         resultMap.forEach((key, value) -> report.add(new Report(key, value)));
         return new PageImpl<>(report);
     }
@@ -81,6 +87,12 @@ public class ArchiveService {
         Matcher m = p.matcher(in);
         boolean matches = m.matches();
         return m.group();
+    }
+
+    private Map<String, Long> orderByValue(LinkedHashMap<String, Long> resultMap) {
+        Stream<Map.Entry<String, Long>> sorted = resultMap.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()));
+        return sorted.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     }
 
 }
